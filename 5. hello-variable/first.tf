ead282591021f430aee422015cd3e0e7b0e3cf76
user.name like : "kushagracdhry@gmail.com <kushagracdhry@gmail.com>
2024-08-26 21:07:04 +0530

    variable, variable value from command, multiple variables

--- 5. hello-variable/first.tf
+++ 5. hello-variable/first.tf
@@ -1,3 +1,12 @@
+# output "printname" {
+#   value = "Hello ${var.username}"
+# }
+
+# terraform plan -var "username=Kush" --> Pass variable value from command
+# ------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
+
 output "printname" {
-  value = "Hello ${var.username}"
+  value = "Hello ${var.username}, your age is ${var.age}"
 }
+
--- 5. hello-variable/variable.tf
+++ 5. hello-variable/variable.tf
@@ -1,1 +1,21 @@
-variable "username" {}
+# variable "username" {
+#     default = "World"
+# }
+
+# terraform plan --> Hello World
+# terraform plan -var "username=Kush"  --> Hello Kush
+
+# ------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
+
+variable username {
+    default = "World"
+}
+
+variable age {
+    default = 20
+}
+
+
+# terraform plan --> Hello World, your age is 20
+# terraform plan -var "username=Kush" -var "age=25" --> Hello Kush, your age is 25
