2bd206df5686f384712cb01c66f76dcb6a24b9e4
user.name like : "kushagracdhry@gmail.com <kushagracdhry@gmail.com>
2024-08-26 19:50:02 +0530

    hello world and bolier plate code

---
+++ hello-world/first.tf
@@ -0,0 +1,14 @@
+# <Block> <Parameters> {
+#   argument1 = value
+#   argument1 = value
+# }
+
+
+# <BLOCK TYPE> "<BLOCK LABEL>" "<BLOCK LABEL>" {
+# # Block body
+# <IDENTIFIER> = <EXPRESSION> # Argument
+# }
+
+output "print" {
+  value = "Hello World!!!"
+}
