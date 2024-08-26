b232ccd0357dd94618bcd87ea8d11b388cf52a3a
user.name like : "kushagracdhry@gmail.com <kushagracdhry@gmail.com>
2024-08-26 20:06:54 +0530

    multiblock tf file added

---
+++ hello-world-multiblock/first.tf
@@ -0,0 +1,11 @@
+output first-block {
+    value = "Hello world from 1st block"
+}
+
+output second-block {
+    value = "Hello world from 2nd block"
+}
+
+output third-block {
+    value = "Hello world from 3rd block"
+}
