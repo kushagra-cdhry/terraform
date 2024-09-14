ca539b3138218eb6d8664b326e4c94e9830757b3
user.name like : "kushagracdhry@gmail.com <kushagracdhry@gmail.com>
2024-09-14 14:25:46 +0530

    variable, pass variable from command, multiple variables, default value of variable

--- 5. hello-variable/variable.tf
+++ 5. hello-variable/variable.tf
@@ -2,20 +2,33 @@
 #     default = "World"
 # }
 
-# terraform plan --> Hello World
-# terraform plan -var "username=Kush"  --> Hello Kush
+# # terraform plan --> Hello World
+# # terraform plan -var "username=Kush"  --> Hello Kush
 
-# ------------------------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------------------------
+# # ------------------------------------------------------------------------------------------------
+# # ------------------------------------------------------------------------------------------------
 
-variable username {
-    default = "World"
-}
+# variable username2 {
+#     default = "World"
+# }
 
-variable age {
-    default = 20
-}
-
+# variable age {
+#     default = 20
+# }
 
 # terraform plan --> Hello World, your age is 20
 # terraform plan -var "username=Kush" -var "age=25" --> Hello Kush, your age is 25
+
+# ------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
+
+# # Variable Types
+variable username3 {
+    type = string
+    default = "World"
+}
+
+variable age2 {
+    type = number
+    default = 20
+}
