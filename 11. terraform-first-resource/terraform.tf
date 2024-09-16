cecda113271d1a037d4160562bbe5c16a023c61a
IAMOPS <IAMOPS@DESKTOP-FP81A5P>
2024-09-16 13:10:01 +0530

    terraform apply, .tfstae file, terraform destroy, .tfstae.backup file

---
+++ 11. terraform-first-resource/terraform.tf
@@ -0,0 +1,19 @@
+provider "github" {
+  token = "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"
+}
+
+resource "github_repository" "terraform-first-repo" {
+  name        = "first-repo-from-terraform"
+  description = "My first repo from terraform"
+
+  visibility = "public"
+  auto_init = true
+}
+
+resource "github_repository" "terraform-second-repo" {
+  name        = "second-repo-from-terraform"
+  description = "My second repo from terraform"
+
+  visibility = "public"
+  auto_init = true
+}
